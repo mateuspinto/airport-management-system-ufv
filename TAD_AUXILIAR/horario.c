c777fd91cff6f9de01f3e8811d4a93c04e58d706
Mateus PS <mateus.p.silva@ufv.br>
2018-09-04 14:06:38 -0300

    Corrigido de o int do unsigned short int --mateusps10

--- TAD_AUXILIAR/horario.c
+++ TAD_AUXILIAR/horario.c
@@ -15,65 +15,65 @@ int horario_inicializa (horario *ponteiro) {
 
 /* Define o dado Hora do TAD horario. Tem como entrada um ponteiro para o TAD e
    um unsigned short para a hora. Retorna 0 se for bem sucedido */
-int horario_setHora(horario *ponteiro, unsigned short hora){
+int horario_setHora(horario *ponteiro, unsigned short int hora){
   ponteiro -> hora = hora;
   return 0;
 }
 
 /* Define o dado Minuto do TAD horario. Tem como entrada um ponteiro para o TAD
    horario e um unsigned short para o minuto. Retorna 0 se for bem sucedido */
-int horario_setMinuto(horario *ponteiro, unsigned short minuto){
+int horario_setMinuto(horario *ponteiro, unsigned short int minuto){
   ponteiro -> minuto = minuto;
   return 0;
 }
 
 /* Define o dado Dia do TAD horario. Tem como entrada um ponteiro para o TAD
    horario e um unsigned short para o dia. Retorna 0 se for bem sucedido */
-int horario_setDia(horario *ponteiro, unsigned short dia){
+int horario_setDia(horario *ponteiro, unsigned short int dia){
   ponteiro -> dia = dia;
   return 0;
 }
 
 /* Define o dado Mes do TAD horario. Tem como entrada um ponteiro para o TAD
    horario e um unsigned short para o mes. Retorna 0 se for bem sucedido */
-int horario_setMes(horario *ponteiro, unsigned short mes){
+int horario_setMes(horario *ponteiro, unsigned short int mes){
   ponteiro -> mes = mes;
   return 0;
 }
 
 /* Define o dado Ano do TAD horario. Tem como entrada um ponteiro para o TAD
    horario e um unsigned short para o ano. Retorna 0 se for bem sucedido */
-int horario_setAno(horario *ponteiro, unsigned short ano){
+int horario_setAno(horario *ponteiro, unsigned short int ano){
   ponteiro -> ano = ano;
   return 0;
 }
 
 /* Retorna o dado Hora do TAD horario. Tem como entrada um ponteiro para o TAD
    horario */
-unsigned short horario_getHora(horario *ponteiro){
+unsigned short int horario_getHora(horario *ponteiro){
   return ponteiro -> hora;
 }
 
 /* Retorna o dado Minuto do TAD horario. Tem como entrada um ponteiro para o TAD
    horario */
-unsigned short horario_getMinuto(horario *ponteiro){
+unsigned short int horario_getMinuto(horario *ponteiro){
   return ponteiro -> minuto;
 }
 
 /* Retorna o dado Dia do TAD horario. Tem como entrada um ponteiro para o TAD
    horario */
-unsigned short horario_getDia(horario *ponteiro){
+unsigned short int horario_getDia(horario *ponteiro){
   return ponteiro -> dia;
 }
 
 /* Retorna o dado Mes do TAD horario. Tem como entrada um ponteiro para o TAD
    horario */
-unsigned short horario_getMes(horario *ponteiro){
+unsigned short int horario_getMes(horario *ponteiro){
   return ponteiro -> mes;
 }
 
 /* Retorna o dado Ano do TAD horario. Tem como entrada um ponteiro para o TAD
    horario */
-unsigned short horario_getAno(horario *ponteiro){
+unsigned short int horario_getAno(horario *ponteiro){
   return ponteiro -> ano;
 }
--- TAD_AUXILIAR/horario.h
+++ TAD_AUXILIAR/horario.h
@@ -8,29 +8,29 @@ definidos */
 
 // Definindo struct do TAD e o declarando como um tipo.
 typedef struct {
-  unsigned short hora;
-  unsigned short minuto;
-  unsigned short dia;
-  unsigned short mes;
-  unsigned short ano;
+  unsigned short int hora;
+  unsigned short int minuto;
+  unsigned short int dia;
+  unsigned short int mes;
+  unsigned short int ano;
 } horario;
 
 // Definindo protótipo da função inicializa que elimina lixo de memória.
 int horario_inicializa(horario *ponteiro);
 
 // Definindo o protótipo das funções set. Retornam 1 se der certo.
-int horario_setHora(horario *ponteiro, unsigned short hora);
-int horario_setMinuto(horario *ponteiro, unsigned short minuto);
-int horario_setDia(horario *ponteiro, unsigned short dia);
-int horario_setMes(horario *ponteiro, unsigned short mes);
-int horario_setAno(horario *ponteiro, unsigned short ano);
+int horario_setHora(horario *ponteiro, unsigned short int hora);
+int horario_setMinuto(horario *ponteiro, unsigned short int minuto);
+int horario_setDia(horario *ponteiro, unsigned short int dia);
+int horario_setMes(horario *ponteiro, unsigned short int mes);
+int horario_setAno(horario *ponteiro, unsigned short int ano);
 
 // Definindo o protótipo das funções get. Retornam o dado requisitado
-unsigned short horario_getHora(horario *ponteiro);
-unsigned short horario_getMinuto(horario *ponteiro);
-unsigned short horario_getDia(horario *ponteiro);
-unsigned short horario_getMes(horario *ponteiro);
-unsigned short horario_getAno(horario *ponteiro);
+unsigned short int horario_getHora(horario *ponteiro);
+unsigned short int horario_getMinuto(horario *ponteiro);
+unsigned short int horario_getDia(horario *ponteiro);
+unsigned short int horario_getMes(horario *ponteiro);
+unsigned short int horario_getAno(horario *ponteiro);
 
 // Fim do define
 #endif // HOARIO_H_INCLUDED
