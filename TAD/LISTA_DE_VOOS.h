fab21e7ad328b403b872ff52ffccf37783546bf9
Mateus PS <mateus.p.silva@ufv.br>
2018-09-05 11:17:24 -0300

    adicionado LISTA_DE_VOOS --mateusps10

--- TAD/LISTA_DE_VOOS.h
+++ TAD/LISTA_DE_VOOS.h
@@ -7,11 +7,16 @@
 
 #include "VOO.h"
 
-typedef struct {
+typedef struct ITEM_LISTAS_DE_VOOS {
     VOO item;
-    ITEM_LISTAS_DE_VOOS *lista;
+    struct ITEM_LISTAS_DE_VOOS *proximoPtr;
 } ITEM_LISTAS_DE_VOOS;
 
+typedef struct {
+    ITEM_LISTAS_DE_VOOS *primeiroPtr;
+    ITEM_LISTAS_DE_VOOS *ultimoPtr;
+    unsigned int numItens;
+} LISTA_DE_VOOS;
 
 
 
