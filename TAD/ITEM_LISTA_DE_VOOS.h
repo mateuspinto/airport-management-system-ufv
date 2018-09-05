77f0cabfbcd07c798086f1aa89b3d143d2bf398c
VinicioHaner <viniciohaner@rocketmail.com>
2018-09-05 15:53:29 -0300

    Adicionei as funcoes do TAD VOO. --Vinicius

---
+++ TAD/ITEM_LISTA_DE_VOOS.h
@@ -0,0 +1,27 @@
+//
+// Created by vburi on 05/09/2018.
+//
+
+#ifndef XAERO_TP1AEDS_LISTA_VOO_H
+#define XAERO_TP1AEDS_LISTA_VOO_H
+
+#include "VOO.h"
+
+typedef struct {
+    VOO item;
+    ITEM_LISTAS_DE_VOOS *lista;
+} ITEM_LISTAS_DE_VOOS;
+
+typedef struct {
+    ITEM_LISTAS_DE_VOOS *primeiroPtr;
+    ITEM_LISTAS_DE_VOOS *ultimoPtr;
+    unsigned int numItens;
+} LISTA_DE_VOOS;
+
+int LISTA_DE_VOOS_criaLista(LISTA_DE_VOOS *ponteiro);
+int LISTA_DE_VOOS_insereVoo(LISTA_DE_VOOS *lista, VOO *item);
+int LISTA_DE_VOOS_removeVoo(LISTA_DE_VOO *lista, unsigned int VID);
+int LISTA_DE_VOOS_procuraVooIdentificador(LISTA_DE_VOO *lista, unsigned int VID);
+
+
+#endif //XAERO_TP1AEDS_LISTA_VOO_H
