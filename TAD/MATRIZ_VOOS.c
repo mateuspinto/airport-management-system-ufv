e355b98908d4c2153acd2f5cdd4e2c5a51b38d37
lazaroleandro <leandro.lazaro@ufv.br>
2018-09-19 07:52:43 -0300

    Leandro

---
+++ TAD/MATRIZ_VOOS.c
@@ -0,0 +1,29 @@
+#include "ITEM_MATRIZ.h"
+#include "MATRIZ_VOOS.h"
+#include "VOO.h"
+#include <stdlib.h>
+#include "../TAD_AUXILIAR/horario.h"
+
+int MATRIZ_VOOS_inicializa(MATRIZ_VOOS *ponteiro){
+    ITEM_MATRIZ_inicializa(&(ponteiro->item_matriz));
+    ponteiro->totalVoos=0;
+    //horario_inicializaHorario(&(ponteiro->data));
+
+    return 0;
+}
+int MATRIZ_VOOS_setVoo(MATRIZ_VOOS *ponteiro, VOO *voo){
+
+}
+int MATRIZ_VOOS_setVoo(MATRIZ_VOOS *ponteiro, VOO *voo){
+    return 0;
+}
+int MATRIZ_VOOS_delVoo(MATRIZ_VOOS *ponteiro, unsigned int *VID);
+int MATRIZ_VOOS_getVoo(MATRIZ_VOOS *ponteiro, unsigned int *VID);
+int MATRIZ_VOOS_showVoosDecolagemPouso(MATRIZ_VOOS *ponteiro, horario *decolagem, horario *pouso);
+int MATRIZ_VOOS_showVoosDecolagem(MATRIZ_VOOS *ponteiro, horario *decolagem);
+int MATRIZ_VOOS_showVoosPouso(MATRIZ_VOOS *ponteiro, horario *pouso);
+int MATRIZ_VOOS_showVoos(MATRIZ_VOOS *ponteiro);
+int MATRIZ_VOOS_showHorarioMaisVoos(MATRIZ_VOOS *ponteiro);
+int MATRIZ_VOOS_showHorarioMenosVoos(MATRIZ_VOOS *ponteiro);
+int MATRIZ_VOOS_showListaAlteracaoMaisRecente(MATRIZ_VOOS *ponteiro);
+int MATRIZ_VOOS_verificarMatrizEsparca(MATRIZ_VOOS *ponteiro);
