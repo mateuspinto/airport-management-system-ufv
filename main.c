4493579dae5d8d8df4a48ca97a5ed7c87a7656bb
mateusps10 <mateus.p.silva@ufv.br>
2018-09-19 12:26:13 -0300

    Main sendo editado --mateusps10

--- main.c
+++ main.c
@@ -11,29 +11,106 @@
 #include "TAD/MATRIZ_VOOS.h"
 
 int main(){
-    lista_encadeada_random mateus;
-    item_lista_encadeada_random *ponteiro;
 
-    lista_encadeada_random_criaLista(&mateus);
+	char input = ' ';
+	char swapString[30];
+	unsigned int swapUnsInt1, swapUnsInt2, swapUnsInt3;
+	
 
-    lista_encadeada_random_insereVID(&mateus, 200);
-    lista_encadeada_random_insereVID(&mateus, 2100);
-    lista_encadeada_random_insereVID(&mateus, 200);
+	printf("##############################################################\n");
+	printf("#        Seja-bem vindo ao xAero Airport System              #\n");
+	printf("##############################################################\n\n");
 
-    ponteiro = mateus.primeiroPtr->proximo;
-    while(ponteiro!=NULL){
-      printf("%d\n", ponteiro->VID);
-      ponteiro = ponteiro->proximo;
-    }
+	printf("O que voce deseja fazer?\n\n");
+	printf("a- Inicializar\n");
+	printf("b- Inserir voo\n");
+	printf("c- Remover voo\n");
+	printf("d- Procurar voo\n");
+	printf("e- Imprimir voos\n");
+	printf("f- Imprimir voos\n");
+	printf("g- Imprimir voos\n");
+	printf("h- Imprimir toda a matriz\n");
+	printf("i- Encontrar faixa de horario de decolagem e previsao de pouso com maior numero de voos cadastrados.\n");
+	printf("j- Encontrar faixa de horario de decolagem e previsão de pouso com menor numero de voos cadastrados.\n");
+	printf("k- Encontrar lista de voos mais recentemente alterada. \n");
+	printf("l- Encontrar lista de voos menos recentemente alterada\n");
+	printf("m- Verificar se matriz e esparca\n");
+	printf("9- MODO ARQUIVO\n");
+	printf("0- Sair do programa\n\n");
 
-    lista_encadeada_random_removeVoo(&mateus, 200);
+	printf("Digite alguma opcao ");
 
-    ponteiro = mateus.primeiroPtr->proximo;
-    while(ponteiro!=NULL){
-      printf("%d\n", ponteiro->VID);
-      ponteiro = ponteiro->proximo;
-    }
-    
-    system("pause");
-    return 0;
+	while(1){
+		scanf("%c", &input);
+		printf("\n");
+
+		switch(input){
+		case 'a':
+
+			break;
+
+		case 'b':
+
+			break;
+		
+		case 'c':
+
+			break;
+		
+		case 'd':
+
+			break;
+		
+		case 'e':
+
+			break;
+		
+		case 'f':
+
+			break;
+		
+		case 'g':
+
+			break;
+		
+		case 'h':
+
+			break;
+		
+		case 'i':
+
+			break;
+		
+		case 'j':
+
+			break;
+		
+		case 'k':
+
+			break;
+		
+		case 'l':
+
+			break;
+		
+		case 'm':
+
+			break;
+		
+		case '9':
+
+			break;
+		
+		case '0': 
+			return 0;
+
+		default: 
+			printf("Digite novamente");
+	}
+	
+	}
+
+	
+	system("pause");
+	return 0;
 }
--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
-gcc -o xaero.exe main.c TAD_AUXILIAR/horario.c TAD_AUXILIAR/lista_encadeada_random.c TAD/VOO.c TAD/LISTA_DE_VOOS.c TAD/ITEM_MATRIZ.c TAD/MATRIZ_VOOS.c -std=c99
+gcc -o xaero.exe main.c TAD_AUXILIAR/horario.c TAD_AUXILIAR/lista_encadeada_random.c TAD/VOO.c TAD/LISTA_DE_VOOS.c -std=c99 -lm -Wall
 pause
