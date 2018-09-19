4d7887885748e2029477b87ab40b58254e0df500
mateusps10 <mateus.p.silva@ufv.br>
2018-09-19 10:52:16 -0300

    Prototipos da item matriz --mateusps10

--- TAD/ITEM_MATRIZ.c
+++ TAD/ITEM_MATRIZ.c
@@ -22,3 +22,54 @@ int ITEM_MATRIZ_setItemMatriz(ITEM_MATRIZ *ponteiro, LISTA_DE_VOOS *item, horari
     return 0;
 }
 
+// Definindo o protótipo das funções set. Retornam 0 se der certo.
+int ITEM_MATRIZ_LISTA_DE_VOOS_VOO_setHorarioDecolagem(ITEM_MATRIZ *ponteiro, unsigned int vid, horario *horario){
+	return 0;
+}
+
+int ITEM_MATRIZ_LISTA_DE_VOOS_VOO_setHorarioPouso(ITEM_MATRIZ *ponteiro, unsigned int vid, horario *horario){
+	return 0;
+}
+
+int ITEM_MATRIZ_LISTA_DE_VOOS_VOO_setAeroportoDecolagem(ITEM_MATRIZ *ponteiro, unsigned int vid, char *aeroporto){
+	return 0;
+}
+
+int ITEM_MATRIZ_LISTA_DE_VOOS_VOO_setAeroportoPouso(ITEM_MATRIZ *ponteiro, unsigned int vid, char *aeroporto){
+	return 0;
+}
+
+int ITEM_MATRIZ_LISTA_DE_VOOS_VOO_setIdentificadorPistaDecolagem(ITEM_MATRIZ *ponteiro, unsigned int vid, unsigned int identificador){
+	return 0;
+}
+
+/* Definindo o protótipo das funções get. Retornam o dado caso seja de um tipo
+   primitivo, retorna void e permite entrar com um ponteiro para armazenar o
+   dado, caso não seja de um tipo primitivo */
+unsigned int ITEM_MATRIZ_LISTA_DE_VOOS_VOO_getVID(ITEM_MATRIZ *ponteiro){
+	return 0;
+}
+
+void ITEM_MATRIZ_LISTA_DE_VOOS_VOO_getHorarioDecolagem(ITEM_MATRIZ *ponteiro, unsigned int vid, horario *horario){
+
+}
+
+void ITEM_MATRIZ_LISTA_DE_VOOS_VOO_getHorarioPouso(ITEM_MATRIZ *ponteiro, unsigned int vid, horario *horario){
+	
+}
+
+
+void ITEM_MATRIZ_LISTA_DE_VOOS_VOO_getAeroportoDecolagem(ITEM_MATRIZ *ponteiro, unsigned int vid, char *aeroporto){
+	
+}
+
+
+void ITEM_MATRIZ_LISTA_DE_VOOS_VOO_getAeroportoPouso(ITEM_MATRIZ *ponteiro, unsigned int vid, char *aeroporto){
+	
+}
+
+
+int ITEM_MATRIZ_LISTA_DE_VOOS_VOO_getIdentificadorPistaDecolagem(ITEM_MATRIZ *ponteiro, unsigned int vid){
+	return 0;
+}
+
